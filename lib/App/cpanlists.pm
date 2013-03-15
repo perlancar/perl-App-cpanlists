5aaf9373ff308e8c9fb4dbcefa74f865f2625b8f
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2013-03-15 14:53:27 +0700

    Initial commit (unfinished)

---
+++ lib/App/cpanlists.pm
@@ -0,0 +1,17 @@
+package App::cpanlists;
+
+use 5.010001;
+use strict;
+use warnings;
+
+# VERSION
+
+1;
+#ABSTRACT: Create personal lists of CPAN modules/authors
+
+=head1 SYNOPSIS
+
+ # See cpanlists script
+
+
+=cut
