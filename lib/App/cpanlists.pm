c9724385ebf2dce66dd9a39cf8004f6b740984d9
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-16 15:07:35 +0700

    [dzil] Make use of DZP::Rinci::AbstractFromMeta

--- lib/App/cpanlists.pm
+++ lib/App/cpanlists.pm
@@ -1,13 +1,19 @@
 package App::cpanlists;
 
+# DATE
+# VERSION
+
 use 5.010001;
 use strict;
 use warnings;
 
-# VERSION
+$SPEC{':package'} = {
+    v => 1.1,
+    summary => 'Create personal lists of CPAN modules/authors',
+};
 
 1;
-#ABSTRACT: Create personal lists of CPAN modules/authors
+#ABSTRACT:
 
 =head1 SYNOPSIS
 
